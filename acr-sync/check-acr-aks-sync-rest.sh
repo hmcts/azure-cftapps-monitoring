0c1ee0ce43d5f500ecc1869cda9f25b2bd349687
luigibk <41112628+luigibk@users.noreply.github.com>
2019-11-26 21:39:27 +0000

    Fix test images exclusion

--- acr-sync/check-acr-aks-sync-rest.sh
+++ acr-sync/check-acr-aks-sync-rest.sh
@@ -17,7 +17,7 @@ for _ns in $all_namespaces
 do
   for _skip_ns in $skip_namespaces; do [ "$_skip_ns" == "$_ns" ] && continue 2; done
   images=$(curl -k --silent -H "Authorization: Bearer $sa_token" https://kubernetes.default.svc.cluster.local/api/v1/namespaces/${_ns}/pods/ --insecure \
-    | jp -u 'join(`"\n"`, items[].spec.containers[].image)' |grep 'prod-' |grep -v '-test:prod-' | sort |uniq)
+    | jp -u 'join(`"\n"`, items[].spec.containers[].image)' |grep 'prod-' |grep -v 'test:prod-' | sort |uniq)
   echo "** Namespace $_ns hosts $(echo $images |wc -w) unique images to check..."
   for _image in ${images}
   do
