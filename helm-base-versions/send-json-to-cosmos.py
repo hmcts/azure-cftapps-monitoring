2afa6a22d60d82be58bc36a5d85067caf7b4f769
nicholas chang <nicholasc001@hotmail.com>
2024-07-25 13:04:34 +0100

    use name inputs on script

--- helm-base-versions/send-json-to-cosmos.py
+++ helm-base-versions/send-json-to-cosmos.py
@@ -2,20 +2,36 @@ import sys
 import uuid
 from azure.cosmos import CosmosClient, PartitionKey
 from azure.identity import DefaultAzureCredential
+import argparse
 
 # Get the command-line arguments
 cosmos_account = "pipeline-metrics"
 cosmos_db = "platform-metrics"
 cosmos_container = "app-helm-chart-metrics"
-chart_name = sys.argv[1]
-namespace = sys.argv[2]
-cluster_name = sys.argv[3]
-deprecated_chart_name = sys.argv[4]
-current_version = sys.argv[5]
-is_deprecated = sys.argv[6]
-is_error = sys.argv[7]
 
-# Define the Cosmos DB endpoint and initialize the Cosmos DB client and container
+parser = argparse.ArgumentParser(description="Process some integers.")
+
+parser.add_argument('--chart_name', type=str, required=True, help='The chart name')
+parser.add_argument('--namespace', type=str, required=True, help='The namespace')
+parser.add_argument('--cluster_name', type=str, required=True, help='The cluster name')
+parser.add_argument('--deprecated_chart_name', type=str, required=True, help='The deprecated chart name')
+parser.add_argument('--current_version', type=str, required=True, help='The current version of the chart')
+parser.add_argument('--is_deprecated', type=str, required=True, help='Deprecation status')
+parser.add_argument('--flag', type=str, required=True, help='A boolean flag')
+parser.add_argument('--is_error', action='store_true', help='A boolean flag indicating an error')
+
+# Parse the arguments
+args = parser.parse_args()
+
+chart_name = args.chart_name
+namespace = args.namespace
+cluster_name = args.cluster_name
+deprecated_chart_name = args.deprecated_chart_name
+current_version = args.current_version
+is_deprecated = args.is_deprecated
+flag = args.flag
+is_error = args.is_error
+
 endpoint = f"https://{cosmos_account}.documents.azure.com:443/"
 credential = DefaultAzureCredential()
 client = CosmosClient(endpoint, credential=credential)
